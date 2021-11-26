e15c6d1a6a5e85ecb69ba51f8c3f268637f7e329
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-11-26 13:22:03 +0330

    Implement node neighbor initialization

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -40,6 +40,69 @@ public class BoardController : MonoBehaviour
             new int[6] {43, 44, 45, 51, 52, 53 },
             };
 
+        int[][] neighborEdgesOfNode = new int[NODES_COUNT][]
+        {
+            new int[2] {0, 6},
+            new int[2] {0, 1},
+            new int[3] {1, 2, 7},
+            new int[2] {2, 3},
+            new int[3] {3, 4, 8},
+            new int[2] {4, 5},
+            new int[2] {5, 9},
+
+            new int[2] {10, 18},
+            new int[3] {10, 11, 6},
+            new int[3] {11, 12, 19},
+            new int[3] {12, 13, 7},
+            new int[3] {13, 14, 20},
+            new int[3] {14, 15, 8},
+            new int[3] {15, 16, 21},
+            new int[3] {16, 17, 9},
+            new int[2] {17, 22},
+
+            new int[2] {23, 33},
+            new int[3] {23, 24, 18},
+            new int[3] {24, 25, 34},
+            new int[3] {25, 26, 19},
+            new int[3] {26, 27, 35},
+            new int[3] {27, 28, 20},
+            new int[3] {28, 29, 36},
+            new int[3] {29, 30, 21},
+            new int[3] {30, 31, 37},
+            new int[3] {31, 32, 22},
+            new int[2] {32, 38},
+
+            new int[2] {39, 33},
+            new int[3] {39, 40, 49},
+            new int[3] {40, 41, 34},
+            new int[3] {41, 42, 50},
+            new int[3] {42, 43, 35},
+            new int[3] {43, 44, 51},
+            new int[3] {44, 45, 36},
+            new int[3] {45, 46, 52},
+            new int[3] {46, 47, 37},
+            new int[3] {47, 48, 53},
+            new int[2] {48, 38},
+
+            new int[2] {54, 49},
+            new int[3] {54, 55, 62},
+            new int[3] {55, 56, 50},
+            new int[3] {56, 57, 63},
+            new int[3] {57, 58, 51},
+            new int[3] {58, 59, 64},
+            new int[3] {59, 60, 52},
+            new int[3] {60, 61, 65},
+            new int[2] {61, 53},
+
+            new int[2] {66, 62},
+            new int[2] {66, 67},
+            new int[3] {67, 68, 63},
+            new int[2] {68, 69},
+            new int[3] {69, 70, 64},
+            new int[2] {70, 71},
+            new int[2] {71, 65},
+        };
+
         tiles = new TileController[TILES_COUNT];
         nodes = new NodeController[NODES_COUNT];
         edges = new EdgeController[EDGES_COUNT];
@@ -50,8 +113,11 @@ public class BoardController : MonoBehaviour
             nodes[i] = new NodeController();
         for (int i = 0; i < EDGES_COUNT; ++i)
             edges[i] = new EdgeController();
+
         for (int i = 0; i < TILES_COUNT; ++i)
             tiles[i].nodes = neighborNodesOfTile[i].Select(n => nodes[n]).ToArray();
+        for (int i = 0; i < NODES_COUNT; ++i)
+            nodes[i].edges = neighborEdgesOfNode[i].Select(e => edges[e]).ToList();
     }
 
     // Update is called once per frame
