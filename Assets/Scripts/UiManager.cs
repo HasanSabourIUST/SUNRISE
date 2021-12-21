ea32641459db47558cee3466c3a7393f2f3f9b22
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-12-21 15:20:58 +0330

    Add text to prompt the current player

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class UiManager : MonoBehaviour
 {
     public Text playerTurnText;
+    public Text promptText;
+    public Game game;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,25 @@ public class UiManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerTurnText.text = "Turn: " + game.currentPlayer;
+        if (game.phase == Game.GamePhase.Start1)
+        {
+            if (game.state == Game.State.PlaceSettlement)
+                promptText.text = "Place your 1st settlement";
+            else if (game.state == Game.State.PlaceRoad)
+                promptText.text = "Place your 1st road";
+        }
+        else if (game.phase == Game.GamePhase.Start2)
+        {
+            if (game.state == Game.State.PlaceSettlement)
+                promptText.text = "Place your 2nd settlement";
+            else if (game.state == Game.State.PlaceRoad)
+                promptText.text = "Place your 2nd road";
+        }
+        else if (game.phase == Game.GamePhase.Middle)
+        {
+            if (game.state == Game.State.Roll)
+                promptText.text = "Roll";
+        }
     }
 }
