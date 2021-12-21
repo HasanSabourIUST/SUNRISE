86c6647e34484b683e2daa759201b82a453ed372
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-12-21 09:19:27 +0330

    Add player turn to UI

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public Text playerTurnText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
